561e373cd9099f0a27e2fa3d930f956eb975630f
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-24 00:00:16 +0300

    EX21

---
+++ EX21/Program.cs
@@ -0,0 +1,18 @@
+﻿// На вход: координаты двух точек (x, y, z)
+// На выход: расстояние между ними
+
+double FindInterval (int aX, int aY, int aZ, int bX, int bY, int bZ)
+{
+    double interval = Math.Sqrt((bX - aX) * (bX - aX) + (bY - aY) * (bY - aY) + (bZ - aZ) * (bZ - aZ));
+    return interval;
+}
+Console.WriteLine("Enter coordinates of the point A: ");
+int aX = Convert.ToInt32(Console.ReadLine());
+int aY = Convert.ToInt32(Console.ReadLine());
+int aZ = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter coordinates of the point B: ");
+int bX = Convert.ToInt32(Console.ReadLine());
+int bY = Convert.ToInt32(Console.ReadLine());
+int bZ = Convert.ToInt32(Console.ReadLine());
+double interval = FindInterval(aX, aY, aZ, bX, bY, bZ);
+Console.WriteLine($"interval between points equal {interval}");
