6199ea549729751ef656492ade86d87ec3d33db8
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-24 00:07:59 +0300

    EX23

---
+++ EX23/Program.cs
@@ -0,0 +1,26 @@
+﻿// На вход: число (N)
+// На выход: таблица кубов чисел от 1 до N
+
+void SquareIt (int number)
+{
+    if (number > 0)
+    {
+        for (int i = 1; i < number; i++)
+        {
+            int square = i * i * i;
+            Console.Write($"{square}, ");
+        }
+        int squareLast = number * number * number;
+        Console.WriteLine($"{squareLast}.");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error");
+        Console.ResetColor();
+    }
+}
+
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+SquareIt(number);
