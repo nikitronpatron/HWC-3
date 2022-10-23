a6f0f4cf58e587a94cac3c2315de060223bdf2a4
nikitronpatron <nikita.borsh9876@gmail.com>
2022-10-24 01:06:31 +0300

    EX19

---
+++ EX19/Program.cs
@@ -0,0 +1,29 @@
+﻿// На ввод: пятизначное число
+// На вывод: палиндром? да/нет
+
+void FindPalindrome (int number)
+{
+    if (number > 9999 && number < 100000)
+    {
+        int one = number % 10;
+        int ten = number / 10 % 10;
+        int hundred = number / 100 % 10;
+        int thousand = number / 1000 % 10;
+        int tenThousand = number / 10000 % 10;
+        if (one == tenThousand && ten == thousand)
+        {
+            Console.WriteLine($"{number} is a palindrome");
+        }
+        else
+        {
+            Console.WriteLine($"{number} isn`t a palindrome");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Error");
+    }
+}
+Console.WriteLine("Enter a five-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+FindPalindrome(number);
